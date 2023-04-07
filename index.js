9dd8ba77e855c7eea19f3fa9ef4fd9c86ab699ee
Moffat <moffat.muga@gmail.com>
2023-04-07 21:03:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+const cats = ["MIlo", "Otis", "Garfield"]
+cats.push("Ralph");
+cats.unshift("Bob");
+cats.pop("Garfield");
+cats.shift("Milo");
+console.log(cats);
+const allCats = [...cats, "Broom"];
+console.log(cats);
+console.log(allCats);
+const myCats = ["Arnold", ...cats];
+console.log(cats);
+console.log(myCats);
+cats.slice(0);
+console.log(cats.slice(0));
+cats.slice(-1)
+console.log(cats);
